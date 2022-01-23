ab933cb433b1c4962c19069cd6e80dc7f7634536
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-23 09:46:19 +0530

    lines added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var gameroad = document.querySelector(".gameroad");
+
+
+
 // on click on startbtn show the road and hide start btn
 // and show the road
 // move the lines on road
@@ -5,6 +9,14 @@
 function showroad(){
     var startbtn = document.querySelector(".startbtn");
     startbtn.classList.toggle("hide");
-    var gameroad = document.querySelector(".gameroad");
     gameroad.classList.toggle("show")
+}
+// lines function
+// we want only 6 lines so we run loop for less than 7 times
+// now we create a div and set the class and give style in css
+for(x=0;x<6;x++){
+    var div = document.createElement("div");
+    div.setAttribute("class","lines");
+    div.style.top = x*150 + "px";
+    gameroad.appendChild(div)
 }
