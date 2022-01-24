ef3fb12b42bfc9113a9ca7648f1df64a92fd356c
NidhiSharma2004 <vidhi6643@gmail.com>
2022-01-24 13:56:33 +0530

    cars images added

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let gameroad = document.querySelector(".gameroad");
-let car = document.querySelector(".car")  
-let player = {speed:5}
+let car = document.querySelector(".car")
+let player = { speed: 5 }
 
 // on click on startbtn show the road and hide start btn
 // and show the road
 // move the lines on road
 
-function showroad(){
+function showroad() {
     let startbtn = document.querySelector(".startbtn");
     startbtn.classList.toggle("hide");
     gameroad.classList.toggle("show");
@@ -18,33 +18,41 @@ function showroad(){
 // now we create a div and set the class and give style in css
 // now every div will started from top so 1st div index 0 and top also 0; but jaise
 // index increase hoga ex.1,2,3 vasie hi top se value bhi increase hogi so that overlaping na ho
-for(x=0;x<6;x++){
+for (x = 0; x < 6; x++) {
     let roadlines = document.createElement("roadlines");
-    roadlines.setAttribute("class","lines");
-    roadlines.y = x*150
+    roadlines.setAttribute("class", "lines");
+    roadlines.y = x * 150
     roadlines.style.top = roadlines.y + "px";
     gameroad.appendChild(roadlines);
 }
 
 //main function
-function gameplay(){
+function gameplay() {
     movelines();
-    moveCars(car); 
+    moveCars(car);
     // gameOver();
     requestAnimationFrame(gameplay);
+}
+
+// collision
+function isCollide(a,b){
+    let aRect = a.getBoundingClientRect();
+    let bRect = b.getBoundingClientRect();
+    return !((aRect.bottom < bRect.bottom))
 }
 //move the lines
 //first select all the lines and run for each mthond on lines and added a value
 //and give the direction that is top and also give condition if y axis se ye 875px 
 //chla jaye means height 875 ho jaye to isne se -900 kr do taki ye vapis top pr chla jaye 
-function movelines(){
+function movelines() {
     let lines = document.querySelectorAll(".lines");
-    lines.forEach(function(line){
-        if(line.y>=875){
+    lines.forEach(function (line) {
+        if (line.y >= 875) {
             line.y -= 900
         }
         line.y += player.speed
-        line.style.top= line.y + "px"
+        line.style.top = line.y + "px"
+        // console.log( `enemycar`,line.style.top = line.y + "px")
     })
 }
 
@@ -52,62 +60,71 @@ function movelines(){
 // firstof all we have to know that ki user window pe konsi ki press kr rha ho or konsi release kr rha hai 
 // we add event lisetener for down the key and specify the that if particular key is pressed then what happen
 // keycode 38 for toparrow and it can find by console e .keycode
-window.addEventListener("keydown",function(e){
+window.addEventListener("keydown", function (e) {
     // console.log(e.keyCode)
-    if(e.keyCode==38){
+    if (e.keyCode == 38) {
         let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
         // console.log(carBottom)
-        if(carBottom<700){
-            car.style.bottom = carBottom+ player.speed +"px"
+        if (carBottom < 700) {
+            car.style.bottom = carBottom + player.speed + "px"
+            console.log(`my car`,carBottom)
         }
     }
-    if(e.keyCode==40){
+    if (e.keyCode == 40) {
         let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
-        if(carBottom>100){
-            car.style.bottom = carBottom - player.speed +"px"
+        if (carBottom > 100) {
+            car.style.bottom = carBottom - player.speed + "px"
         }
     }
-    if(e.keyCode==39){
+    if (e.keyCode == 39) {
         let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
         // console.log(carleft)
-        if(carleft<450){
-            car.style.left = carleft + player.speed +"px"
+        if (carleft < 450) {
+            car.style.left = carleft + player.speed + "px"
         }
     }
-    if(e.keyCode==37){
+    if (e.keyCode == 37) {
         let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
-        if(carleft>0){
-            car.style.left = carleft - player.speed +"px"
+        if (carleft > 0) {
+            car.style.left = carleft - player.speed + "px"
         }
     }
 })
+
+let enemyCarArr = ["images/car1.png","images/car3.png","images/car4.png"]
 // enemy cars
-for(x=0;x<5;x++){
+for (x = 0; x < enemyCarArr.length; x++) {
     let div = document.createElement("div");
-    div.setAttribute("class","enemyCars");
-    div.y = x*150
+    div.setAttribute("class", "enemyCars");
+    let img = document.createElement("img");
+    img.src = enemyCarArr[x];
+    div.appendChild(img)
+    div.y = x * 190
     div.style.top = div.y + "px";
     gameroad.appendChild(div);
 }
 // move the enemyCars
 //same as line move yha pr hr baar alg alg postion ke liye math functon ke use krenge
-function moveCars(car){
+function moveCars(car) {
     let enemyCars = document.querySelectorAll(".enemyCars");
-    enemyCars.forEach(function(enemyCar){
-        // if(isCollide(car,enemyCar))
-        if(enemyCar.y>=825){
-           
-            enemyCar.y -= 890;
-            enemyCar.style.left = Math.ceil(Math.random()*3)*130+"px"
+    enemyCars.forEach(function (enemyCar) {
+        if(isCollide(car,enemyCar)){
+            console.log("hit")
         }
-        enemyCar.y+= player.speed
-        enemyCar.style.top= enemyCar.y + "px";
+        if (enemyCar.y >= 825) {
+            // if(isCollide(car,enemyCar))
+            enemyCar.y -= 930;
+            enemyCar.style.left = Math.ceil(Math.random() * 130) + "px"
+            console.log( enemyCar.style.left = Math.floor(Math.random()*150) * 3 + "px")
+        }
+        enemyCar.y += player.speed
+        enemyCar.style.top = enemyCar.y + "px";
     })
 }
 
 // game over
 
-function isCollide(a,b){
+function isCollide(a, b) {
     aRect = a.getBoundingClientRect();
     bRect = b.getBoundingClientRect();
 }
