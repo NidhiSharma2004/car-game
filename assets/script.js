b3e6251b83ba716ea707872421e8b06b203a494e
NidhiSharma2004 <vidhi6643@gmail.com>
2022-02-09 11:22:48 +0530

    music fixed

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ let scorevalue = document.querySelector("#scorevalue");
 let highScore = document.querySelector(".highScore");
 let fire = document.querySelector(".fire");
 let gameCountDown = document.querySelector(".gameCountDown");
+let audio = document.getElementById("audio");
 let player = { speed: 5, score: 0 };
 let keys = {
   ArrowUp: false,
@@ -20,16 +21,9 @@ let carSpeed = { Cspeed: 15 };
 let score = 0;
 let carZindex = "";
 let autoLeft = "";
-let audio = new Audio();
-let sound = [
-  "assets/sounds/carStart.mp3",
-  "./assets/sounds/carSoundAcc.m4a",
-  "./assets/sounds/car crash.mp3",
-  "./assets/sounds/carSound.wav",
-];
+let audioElement = new Audio();
+let sound = ["assets/sounds/carStart.mp3", "./assets/sounds/car crash.mp3"];
 let windowHeight = window.innerHeight;
-console.log(windowHeight);
-
 if (windowHeight < 1080) {
   player.speed -= 0;
 } else if (windowHeight < 1440 && windowHeight > 1080) {
@@ -137,111 +131,7 @@ for (x = 0; x < rightHomeArr.length; x++) {
     createRightHandHome(350);
   }
 }
-//keys
-// firstof all we have to know that ki user window pe konsi ki press kr rha ho or konsi release kr rha hai
-// we add event lisetener for down the key and specify the that if particular key is pressed then what happen
-// keycode 38 for toparrow and it can find by console e .keycode
 
-function keyfunction(e) {
-  let gameroadHeight = gameroad.getBoundingClientRect().height;
-  let gameroadWidth = gameroad.getBoundingClientRect().width;
-  let carheight = parseInt(getComputedStyle(car).getPropertyValue("height"));
-  let carWidth = parseInt(getComputedStyle(car).getPropertyValue("width"));
-  e.preventDefault();
-  keys[e.key] = true;
-  let carposition = car.getBoundingClientRect();
-  console.log(carWidth);
-
-  function IncZindexOfCar() {
-    car.style.zIndex = 4;
-    carZindex = car.style.zIndex;
-    car.style.marginLeft = -32 + "px";
-  }
-  function DecZindexOfCar() {
-    car.style.zIndex = 1;
-    car.style.marginLeft = -18 + "px";
-    carZindex = car.style.zIndex;
-  }
-  if (keys[5]) {
-    let carWidth = parseInt(getComputedStyle(car).getPropertyValue("width"));
-    if (windowHeight < -1440) {
-      // first of all here carWidth's value came only once at when we call fun keyup and it's 61 so write cond
-      // but here to decrease size of car i don't have value of current size of car becoz width take value at once
-      // so i have to assign value of current size of car to variable that is (IncreaseCarSize current value=81) and
-      // after 1000s run the fun. that make car same size as before in this i can use only IncreaseCarSize not carWidth
-      if (carWidth < 81) {
-        console.log("statement run  " + carWidth);
-        car.style.width = 81 + "px";
-        IncZindexOfCar();
-        IncreaseCarSize = parseInt(car.style.width);
-      }
-
-      setTimeout(() => {
-        console.log("setTimeout function run");
-        if (IncreaseCarSize == 81) {
-          car.style.width = 61 + "px";
-          DecZindexOfCar();
-        }
-      }, 1000);
-    } else {
-      if (carWidth < 155) {
-        car.style.width = 155 + "px";
-        IncZindexOfCar();
-        NextIncreaseCarSize = parseInt(car.style.width);
-      }
-
-      setTimeout(() => {
-        if (NextIncreaseCarSize == 155) {
-          car.style.width = 135 + "px";
-          DecZindexOfCar();
-        }
-      }, 1000);
-    }
-  }
-  if (keys.ArrowUp) {
-    let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
-
-    if (carBottom < gameroadHeight - carheight - 40) {
-      car.style.bottom = carBottom + carSpeed.Cspeed + "px";
-    }
-  }
-  if (keys.ArrowDown) {
-    let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
-    console.log(carBottom);
-    if (carBottom > 48) {
-      car.style.bottom = carBottom - carSpeed.Cspeed + "px";
-    }
-  }
-  if (keys.ArrowRight) {
-    let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
-    if (carleft < gameroadWidth - carWidth - 20) {
-      car.style.left = carleft + carSpeed.Cspeed + "px";
-    }
-  }
-  if (keys.ArrowLeft) {
-    let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
-    if (carleft > 0) {
-      car.style.left = carleft - carSpeed.Cspeed + "px";
-    }
-  }
-}
-
-function keyUpfunction(e) {
-  e.preventDefault();
-  keys[e.key] = false;
-}
-// collision detection
-function iscollide(a, b) {
-  aRect = a.getBoundingClientRect();
-  bRect = b.getBoundingClientRect();
-  return (
-    aRect.top < bRect.bottom &&
-    aRect.bottom > bRect.top &&
-    aRect.right > bRect.left &&
-    aRect.left < bRect.right &&
-    carZindex == 1
-  );
-}
 //move the lines
 //first select all the lines and run for each mthond on lines and added a value
 //and give the direction that is top and also give condition if y axis se ye 875px
@@ -367,14 +257,11 @@ function moveCars(car, xheight, xheight2, xheight3, xheight4) {
   enemyCars.forEach(function (enemyCar) {
     let gameroadHeight = gameroad.getBoundingClientRect().height;
     if (iscollide(car, enemyCar)) {
-      if ((audio.src = sound[1])) {
-        audio.pause();
-      }
-      // audio.muted=false
-      audio.src = sound[2];
-      audio.muted=true;
-      audio.play();
-      audio.muted=false;
+      audio.pause();
+      audioElement.src = sound[1];
+      audioElement.muted = true;
+      audioElement.play();
+      audioElement.muted = false;
       player.start = false;
       showScore.innerHTML = `you loose <br> your score is ${score + 1}`;
       scorevalue.value = score + 1;
@@ -400,6 +287,111 @@ function moveCars(car, xheight, xheight2, xheight3, xheight4) {
   });
 }
 
+// collision detection
+function iscollide(a, b) {
+  aRect = a.getBoundingClientRect();
+  bRect = b.getBoundingClientRect();
+  return (
+    aRect.top < bRect.bottom &&
+    aRect.bottom > bRect.top &&
+    aRect.right > bRect.left &&
+    aRect.left < bRect.right &&
+    carZindex == 1
+  );
+}
+//keys
+// firstof all we have to know that ki user window pe konsi ki press kr rha ho or konsi release kr rha hai
+// we add event lisetener for down the key and specify the that if particular key is pressed then what happen
+// keycode 38 for toparrow and it can find by console e .keycode
+
+
+function keyfunction(e) {
+  let gameroadHeight = gameroad.getBoundingClientRect().height;
+  let gameroadWidth = gameroad.getBoundingClientRect().width;
+  let carheight = parseInt(getComputedStyle(car).getPropertyValue("height"));
+  let carWidth = parseInt(getComputedStyle(car).getPropertyValue("width"));
+  e.preventDefault();
+  keys[e.key] = true;
+  let carposition = car.getBoundingClientRect();
+  function IncZindexOfCar() {
+    car.style.zIndex = 4;
+    carZindex = car.style.zIndex;
+    car.style.marginLeft = -32 + "px";
+  }
+  function DecZindexOfCar() {
+    car.style.zIndex = 1;
+    car.style.marginLeft = -18 + "px";
+    carZindex = car.style.zIndex;
+  }
+  if (keys[5]) {
+    let carWidth = parseInt(getComputedStyle(car).getPropertyValue("width"));
+    if (windowHeight < -1440) {
+      // first of all here carWidth's value came only once at when we call fun keyup and it's 61 so write cond
+      // but here to decrease size of car i don't have value of current size of car becoz width take value at once
+      // so i have to assign value of current size of car to variable that is (IncreaseCarSize current value=81) and
+      // after 1000s run the fun. that make car same size as before in this i can use only IncreaseCarSize not carWidth
+      if (carWidth < 81) {
+        console.log("statement run  " + carWidth);
+        car.style.width = 81 + "px";
+        IncZindexOfCar();
+        IncreaseCarSize = parseInt(car.style.width);
+      }
+
+      setTimeout(() => {
+        console.log("setTimeout function run");
+        if (IncreaseCarSize == 81) {
+          car.style.width = 61 + "px";
+          DecZindexOfCar();
+        }
+      }, 1000);
+    } else {
+      if (carWidth < 155) {
+        car.style.width = 155 + "px";
+        IncZindexOfCar();
+        NextIncreaseCarSize = parseInt(car.style.width);
+      }
+
+      setTimeout(() => {
+        if (NextIncreaseCarSize == 155) {
+          car.style.width = 135 + "px";
+          DecZindexOfCar();
+        }
+      }, 1000);
+    }
+  }
+  if (keys.ArrowUp) {
+    let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
+
+    if (carBottom < gameroadHeight - carheight - 40) {
+      car.style.bottom = carBottom + carSpeed.Cspeed + "px";
+    }
+  }
+  if (keys.ArrowDown) {
+    let carBottom = parseInt(getComputedStyle(car).getPropertyValue("bottom"));
+    console.log(carBottom);
+    if (carBottom > 48) {
+      car.style.bottom = carBottom - carSpeed.Cspeed + "px";
+    }
+  }
+  if (keys.ArrowRight) {
+    let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
+    if (carleft < gameroadWidth - carWidth - 20) {
+      car.style.left = carleft + carSpeed.Cspeed + "px";
+    }
+  }
+  if (keys.ArrowLeft) {
+    let carleft = parseInt(getComputedStyle(car).getPropertyValue("left"));
+    if (carleft > 0) {
+      car.style.left = carleft - carSpeed.Cspeed + "px";
+    }
+  }
+}
+
+function keyUpfunction(e) {
+  e.preventDefault();
+  keys[e.key] = false;
+}
+
 // if player is true then start the animations
 function gameplay() {
   let gameroadPositon = gameroad.getBoundingClientRect();
@@ -420,15 +412,22 @@ function gameplay() {
 
 // startgame game fun will run only if player is true and call the function game play
 // cif collisio then player will be false so don't call the game play fun
+// do this to avoid uncaught promises of audio ===>
+// audioElement.muted = true;
+// audioElement.src = sound[0];
+// audioElement.play();
+// audioElement.muted=false
 
+// NOTE : i want to play accelerator sound until the car is collide so i have to loop the accelerator audio
+// so i have created audio element in html and loop this and when collsion occur i paused the audio of accelerator
 function startgame() {
   gameCountDown.classList.add("show");
   player.start = true;
   car.style.zIndex = 1;
-  audio.muted = true;
-  audio.src = sound[0];
-  audio.play();
-  audio.muted=false
+  audioElement.muted = true;
+  audioElement.src = sound[0];
+  audioElement.play();
+  audioElement.muted = false;
   carZindex = car.style.zIndex;
   gameroad.classList.add("show");
   startbtn.classList.add("hide");
@@ -437,11 +436,12 @@ function startgame() {
 
   setTimeout(() => {
     window.addEventListener("keydown", keyfunction);
-    // audio.muted=false
-    audio.src = sound[1];
-    audio.muted=true;
+    audioElement.muted = false;
+    audioElement.src = sound[1];
+    audioElement.play();
+    audioElement.muted = true;
+
     audio.play();
-    audio.muted=false
     window.requestAnimationFrame(gameplay);
   }, 3000);
 }
